a1a4fce4d8bdb49b2289a26d71eb33febc401bd6
Donatien <donatien.linossier@gmail.com>
2023-12-19 17:50:54 +0100

    constant.sh  - Creation note.txt - Ideas shellD.sh - Process L & cleaning truckAnalyse.sh - Merge, cleaning, process D1/D2/L, modularity

--- shellD.sh
+++ shellD.sh
@@ -1,51 +1,19 @@
 #!/bin/bash
 temp=temp/
 
-
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <filename>"
-    exit 1
-fi
-
-
-
-filename="data/$1"
-
-if [ -f "$filename" ]; then
-    echo "File $filename exists."
-else
-    echo "File $filename does not exist."
-    exit 1
-fi 
-
-
-
-traitement_D1() {
-    echo lancement du traitement D1...
-    awk -F';' '{printf "%s;%d\n", $6, $1}' sample.csv | tail -n +2 > temp/tmpD1.csv
-    ./progc/main
-}
-
-
-
-traitement_D2() {
-    echo lancement du traitement D2...
-    awk -F ';' 'BEGIN{OFS=";"; LC_NUMERIC="C"} {printf "%s;%.3f\n", $6, $5}' sample.csv | tail -n +2 > temp/tmpD2.csv
-}
-
-D1() {
-    echo lancement du traitement D1 without c...
+processD1() {
+    echo Process D1 on file $1
     now=`date +%s`
     
     tail +2 $1 | awk -F';' '!seen[$1,$6]++ {count[$6]++} END {for (driver in count) print count[driver], driver}' | sort -k1nr | head -10 > temp/D1.txt
 
     nowB=`date +%s`
-    echo $((nowB - now))
-    cat < D1.txt
+    cat < temp/D1.txt
+    echo "Process lasted $((nowB - now)) seconds."
 }
 
-D2() {
-    echo lancement du traitement D2 without c...
+processD2() {
+    echo Process D2 on file $1
     now=`date +%s`
     
     tail +2 $1 | awk -F';' '{sum[$6]+=$5} END {for (driver in sum) print sum[driver], driver}' | sort -k1nr | head -10 > temp/D2.txt
@@ -53,31 +21,22 @@ D2() {
     nowB=`date +%s`
 
     cat < temp/D2.txt
-    echo $((nowB - now))
+    echo "Process lasted $((nowB - now)) seconds."
 }
 
-
-#Equivalent speed as D2Bis
-D2Bis() {
-    # Echo a message indicating the start of the processing
-    echo "lancement du traitement D2 without c..."
-
-    # Record the current timestamp
+processL() {
+    echo Process L on file $1
     now=`date +%s`
+    
+    tail +2 $1 | awk -F';' '{sum[$1]+=$5} END {for (driver in sum) print sum[driver], driver}' | sort -k1nr | head -10 > temp/D3.txt
 
-    # Use cut to extract columns 6 and 5 from data.csv and save the result in ff.txt
-    tail +2 $1 | cut -d ';' -f 5,6 | awk -F ';' '{count[$2]+=$1} END {for (elt in count) print elt, count[elt]}' | sort -k3nr | head -10 > temp/D2Bis.csv
-
-    # Record the current timestamp again
     nowB=`date +%s`
 
-    # Display the contents of a file named D2.txt (This should be ff.txt instead based on your script)
-    cat < temp/D2Bis.txt
-
-    # Calculate and echo the time taken for processing
-    echo $((nowB - now))
+    cat < temp/D3.txt
+ 
+    echo "Process lasted $((nowB - now)) seconds."
 }
 
-D2 $filename
+#D2 $filename
 #D1 awk -F';' '!seen[$1,$6]++ {count[$6]++} END {for (driver in count) print count[driver], driver}' data.csv | sort -k1,1nr > driver_routes_sorted.txt
 #D2 awk -F';' '{sum[$6]+=$5} END {for (driver in sum) print sum[driver], driver}' data.csv | sort -k1,1nr > driver_distance_sorted.txt
