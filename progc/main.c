df6d7f66f6ec0146e8e9ea051df6ea4d93b112ee
Donatien <donatien.linossier@gmail.com>
2023-12-21 10:43:12 +0100

    main.c - Preparation of data for process T(To test)

--- progc/main.c
+++ progc/main.c
@@ -3,7 +3,64 @@
 #include <string.h>
 #include <unistd.h>
 
-typedef struct D1 {
+void getDataForT(const char* input, char* cityA, char* cityB, char* isFirst) {
+    char* token = strtok((char*)input, ";");//route ID
+
+    token = strtok((char*)input, ";");
+    *isFirst = atoi(token);
+
+    token = strtok((char*)input, ";");
+    cityA = strdup(token);(token);
+
+    token = strtok((char*)input, ";");
+    cityB = strdup(token);(token);
+}
+
+int main() {
+
+    char bufferb[4096]; // Assuming a reasonable buffer size
+
+    if (getcwd(bufferb, sizeof(bufferb)) != NULL) {
+        printf("Current working directory: %s\n", bufferb);
+    } else {
+        perror("getcwd() error");
+        return 1;
+    }
+
+    FILE *fptr;
+
+    // Open a file in read mode
+    char buffer[100];
+    fptr = fopen("sample.csv", "r"); 
+    //fptr = fopen("temp/tmpD1.csv", "r"); 
+    if(fptr==NULL) {
+        printf("NULL");
+        exit(0);
+    }
+
+    char* cityA;
+    char* cityB;
+    char isFirst;
+    while (fgets(buffer, sizeof(buffer), fptr) != NULL) {
+        getDataForT(buffer, cityA, cityB, &isFirst);
+        printf("%s %s %c", cityA, cityB, isFirst);
+
+    }
+    
+    /*while (fgets(buffer, sizeof(buffer), fptr) != NULL) {
+        D1* d1 = createD1fromLine(buffer);
+        printf("%s %d\n", d1->driverName, d1->routeID);
+
+    }*/
+    
+    fclose(fptr);
+    return 0;
+}
+
+
+
+
+/*typedef struct D1 {
     char* driverName;
     int routeID;
 } D1;
@@ -38,36 +95,4 @@ D2* createD2fromLine(const char* input) {
     d2->distance = atoi(token);
 
     return d2;
-}
-
-int main() {
-
-    char bufferb[4096]; // Assuming a reasonable buffer size
-
-    if (getcwd(bufferb, sizeof(bufferb)) != NULL) {
-        printf("Current working directory: %s\n", bufferb);
-    } else {
-        perror("getcwd() error");
-        return 1;
-    }
-
-    FILE *fptr;
-
-    // Open a file in read mode
-    char buffer[100];
-    fptr = fopen("temp/tmpD1.csv", "r"); 
-    if(fptr==NULL) {
-        printf("NULL");
-        exit(0);
-    }
-
-    
-    while (fgets(buffer, sizeof(buffer), fptr) != NULL) {
-        D1* d1 = createD1fromLine(buffer);
-        printf("%s %d\n", d1->driverName, d1->routeID);
-
-    }
-    
-    fclose(fptr);
-    return 0;
-}
+}*/
