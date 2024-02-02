d19895cd50026f66c047befeef19c4eb5b0227eb
Antonin <bo.antonin@gmail.com>
2024-02-02 17:53:44 +0100

    commentaring

--- trucksanalyse.sh
+++ trucksanalyse.sh
@@ -16,44 +16,44 @@ if [ "$#" -eq 0 ]; then
     exit 1
 fi
 
-#verrification 
-
+# Verification if the source file exists; if not, display an error message and print help.
 if [ ! -f "$1" ]; then
     echo "File $1 does not exist."
-    printHelp 1 #put 1 in parrameter for exit
+    printHelp 1 # Put 1 as a parameter to indicate exit.
 else
-    cp $1 "$dataFolder/$dataname"
+    cp "$1" "$dataFolder/$dataname"
 fi
+
+# Set the full path of the file in the variable filename.
 filename="$dataFolder/$dataname"
 
-
+# Declaration and initialization of the settingsVar array.
 settingsVar=(0 0 0 0 0 0)
 
-#take arguments
+# Take the arguments.
 for arg in "$@"; do
     case $arg in
-            "-h")
+        "-h")
             settingsVar[0]=1 ;;
-            "-d1")
+        "-d1")
             settingsVar[1]=1 ;;
-            "-d2") 
+        "-d2") 
             settingsVar[2]=1 ;;
-            "-l") 
+        "-l") 
             settingsVar[3]=1 ;;
-            "-t") 
+        "-t") 
             settingsVar[4]=1 ;;
-            "-s") 
+        "-s") 
             settingsVar[5]=1 ;;    
     esac
 done
 
-
-
-#Choice
+# Choice of actions to be performed based on the passed parameters.
 if [ "${settingsVar[0]}" -eq 1 ]; then
     printHelp 1
 else 
-    if ((${settingsVar[4]} == 1 || ${settingsVar[5]} == 1)); then #we compile for the modes who need c to be run
+    # Compilation if necessary.
+    if ((${settingsVar[4]} == 1 || ${settingsVar[5]} == 1)); then # We compile for the modes that need c to be executed.
         if [ ! -f ".$EXECUTABLE" ]; then
             echo Compilation
             make cleanCompilation -s
@@ -90,16 +90,3 @@ else
         processS $filename
     fi
 fi
-
-
-
-
-
-
-
-
-
-
-
-
-
